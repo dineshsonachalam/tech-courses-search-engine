9c8e03b3c911e4dde7f8c6175ea9b1a6e015e0dc
Dinesh Sonachalam <dineshsonachalam@gmail.com>
2021-05-07 09:32:11 +0530

    Updated doc

--- frontend/cypress/integration/search-courses.spec.js
+++ frontend/cypress/integration/search-courses.spec.js
@@ -5,45 +5,25 @@ describe("Search courses", () => {
     })
     
     it('Type the course', () => {
-        cy.get('#rc_select_0').type('Python').type('{enter}')
+        cy.get('#rc_select_0').type('react').type('{enter}')
         cy.wait(5000)
     })
-    
+     
     it('Compare search result', () => {
         cy.wait(10000)
         let expected_courses = [
                         {
-                            "title": "Google's Python Class",
-                            "upvotes": "213",
-                            "topic": "Python"
+                            "title": "React Crash Course 2018 - React Tutorial with Examples",
+                            "upvotes": "78",
+                            "topic": "React"
                         },
                         {
-                            "title": "Complete Python Bootcamp",
-                            "upvotes": "196",
-                            "topic": "Python"
-                        },
-                        {
-                            "title": "Automate the Boring Stuff with Python",
-                            "upvotes": "93",
-                            "topic": "Python"
-                        },
-                        {
-                            "title": "Official Python Tutorial",
-                            "upvotes": "74",
-                            "topic": "Python"
-                        },
-                        {
-                            "title": "Working with Strings in Python",
-                            "upvotes": "4",
-                            "topic": "Python"
-                        },
-                        {
-                            "title": "Learn Python the Hard Way",
-                            "upvotes": "293",
-                            "topic": "Python"
+                            "title": "Awesome React",
+                            "upvotes": "42",
+                            "topic": "React"
                         }
         ];
-        for (var index=0; index < 6; index++) {   
+        for (var index=0; index < expected_courses.length; index++) {   
             const index_pos = index;
             cy.get(`:nth-child(${index_pos+1}) > .ant-list-item-meta > .ant-list-item-meta-content > .ant-list-item-meta-title > a`).then(($title_elem) => {
                 let title = $title_elem.text();
@@ -58,7 +38,7 @@ describe("Search courses", () => {
             cy.get(`:nth-child(${index_pos+1}) > .ant-list-item-action > :nth-child(2) > .ant-tag > :nth-child(2)`).then(($topic_elem) => {
                 let topic = $topic_elem.text();
                 let expected_topic = expected_courses[index_pos]["topic"];
-                expect(topic).to.deep.eq(expected_topic)
+                expect(topic).to.deep.eq(expected_topic);
             })
         }
     })
